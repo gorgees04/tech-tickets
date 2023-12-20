0e2c0a7242c014ba1cc8603ab14289f577404805
Gorgees Odisho <gorgees041@gmail.com>
2023-12-20 23:11:07 +0300

    connect to database & create a schema #5

---
+++ app/libs/database/database.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let isConnected = false; // to track the connection
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+
+  // if it's not corrected
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "tickets",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
