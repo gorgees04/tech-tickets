79bd85125a3666102ac2c57ed458677dacbf1a48
Gorgees Odisho <gorgees041@gmail.com>
2023-12-21 22:30:42 +0300

    sort tickets acording to categories #12

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Content from "@/app/ui/home/Content";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col p-10">
+    <main className="flex min-h-screen flex-col m-2">
       <div className="w-full text-center">
         <h1 className="text-4xl font-bold m-3">Tickets</h1>
       </div>
