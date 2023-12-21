a3433b389fca3e06b5d5111fe6c5928ef4f96139
Gorgees Odisho <gorgees041@gmail.com>
2023-12-21 03:16:53 +0300

    change fetch method to useEffect to fetch the data immeditly after submit #5

--- app/ui/home/Content.tsx
+++ app/ui/home/Content.tsx
@@ -1,25 +1,30 @@
+"use client";
 import { TicketCard } from "@/app/libs/definitions";
 import Crad from "./card/Card";
+import { useEffect, useState } from "react";
 
-const getData = async () => {
-  try {
-    const res = await fetch("http://localhost:3000/api/tickets", {
-      cache: "no-store",
-    });
+export default function Content() {
+  // tickets
+  const [tickets, setTickets] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const getData = async () => {
+      setLoading(true);
+      const res = await fetch("http://localhost:3000/api/tickets");
+      const data = await res.json();
+      setTickets(data);
+      setLoading(false);
+    };
+    getData();
+  }, []);
 
-    if (!res.ok) {
-      throw new Error("Couldn't fetch the data");
-    }
-
-    return res.json();
-  } catch (error) {
-    throw `Failed to fetch the data from the api // ${error}`;
+  if (loading) {
+    return (
+      <div className="text-3xl text-center mt-[300px]">
+        <p>Loading Tickets....</p>
+      </div>
+    );
   }
-};
-
-export default async function Content() {
-  const tickets = await getData();
-
   return (
     <div className="w-full flex flex-wrap items-center justify-center">
       {tickets.map((ticket: TicketCard) => {
